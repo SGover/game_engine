96bdae9ab5add0d2f9d931add075151c81d03211
SGover <SGovertime@gmail.com>
2013-10-30 16:32:06 +0200

    a test

---
+++ controls.py
@@ -0,0 +1,10 @@
+class control():
+    
+    def __init__(self):
+        pass
+    
+    def setSpatial(self.spatial):
+        self.spatial=spatial
+    
+    def update():
+        pass
---
+++ spatials.py
@@ -0,0 +1,39 @@
+import settings
+import controls
+time_interval=0.001*1000/settings.FPS
+
+    
+class spatial():
+    def __init__(self,rect, z, color):
+        self.posZ= z
+        self.rect = rect
+        self.color = color
+    def draw(self, surf):
+        pygame.draw.rect(surf, self.color, self.rect, width=0)
+    def on_screen( minX, maxX, minY, maxY):
+        if rect.right<minX or rect.left>maxX :
+            return False
+        if rect.top<minY or rect.bottom>maxY :
+            return False
+        return True
+    
+class dynSpatial(spatial):    
+        def __init__(self,rect,z, color):
+            spatial.__init__(self,rect,z,color)            
+            self.v=(0,0)
+            self.a=(0,0)            
+            controls=[]
+        def addControl(control):
+            control.setSpatial(self)
+            controls.append(control)
+        def update(self):
+            self.v+=a*interval
+            self.rect.x+=self.v[0]*time_interval
+            for control in controls:
+                control.update()
+        def stop(self):
+            self.v=(0,0)
+            self.a=(0,0)
+
+        
+
